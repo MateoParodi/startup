92c806efa4d0843b1b33fe1cd9619f444f70d67d
Mateo Parodi <matthparodi@gmail.com>
2016-06-06 13:31:07 -0300

    Added Ajax Repository search practice

---
+++ 01-JS-DOM-APIs/AjaxRepository/script.js
@@ -0,0 +1,26 @@
+function getRepository(){
+
+
+    var containerList = document.getElementById("repoList");
+
+    $.ajax({
+        url: "https://api.github.com/search/repositories",
+        type: 'GET',
+        data: "q=" + document.getElementById("repo").value,
+
+
+        success: function(data){
+            $.each(data.items, function(x, repo){
+                var li = document.createElement("li");
+                containerList.appendChild(li);
+                var data = document.createTextNode(repo.full_name);
+                li.appendChild(data);
+            });
+
+        },
+        error: function(response){
+            alert(response);
+        }
+    });
+
+}
