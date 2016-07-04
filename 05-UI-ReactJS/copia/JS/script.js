7ac10b99ceddd828669334df0aa39b313faa49fa
Mateo Parodi <matthparodi@gmail.com>
2016-07-04 04:58:19 -0300

    Bugs Fixed / Edit function working

--- 05-UI-ReactJS/copia/JS/script.js
+++ 05-UI-ReactJS/copia/JS/script.js
@@ -1,8 +1,8 @@
 var moviesObject = [
-    {id: 1, title: "Titanic", year: 1997, duration: 142, description: 'A gigant boat that sinks in the ocean'},
-    {id: 2, title: "Toy Story", year: 1995, duration: 154, description: 'Some toys playing around'},
-    {id: 3, title: "Inglourious Basterds", year: 2009, duration: 139, description: 'A group of americans killing nazis'},
-    {id: 4, title: "Fight Club", year: 1999, duration: 81, description: 'Some freak guy messing around'}
+    {title: "Titanic", year: 1997, duration: 142, description: 'A gigant boat that sinks in the ocean'},
+    {title: "Toy Story", year: 1995, duration: 154, description: 'Some toys playing around'},
+    {title: "Inglourious Basterds", year: 2009, duration: 139, description: 'A group of americans killing nazis'},
+    {title: "Fight Club", year: 1999, duration: 81, description: 'Some freak guy messing around'}
 ];
 
 /**
@@ -27,8 +27,16 @@ var MoviesBox = React.createClass({
         moviesObject.splice(index, 1);
         this.setState({movies: moviesObject});
     },
-    handleMovieUpdate: function (movie) {
-        console.log(movie);
+    handleMovieUpdate: function (movieToUpdate, movieOld) {
+        for (var i = 0; i < moviesObject.length; i++) {
+            if (moviesObject[i].title === movieOld.title) {
+                var movieSelected = moviesObject[i];
+            }
+        }
+
+        var index = moviesObject.indexOf(movieSelected);
+        moviesObject.splice(index, 1, movieToUpdate);
+        this.setState({movies: moviesObject});
     },
 
     render: function () {
@@ -50,16 +58,15 @@ var MoviesList = React.createClass({
         return this.props.del(movieTile);
     },
 
-    handleUpdate: function (movie) {
-        this.props.onMovieUpdate(movie)
+    handleUpdate: function (movieUp, movieOld) {
+        this.props.onMovieUpdate(movieUp, movieOld)
 
     },
 
     render: function () {
-        var moviesNodes = this.props.movies.map(function (movie) {
-            var handle_update = this.handleUpdate.bind(this, movie);
+        var moviesNodes = this.props.movies.map(function (movie, i) {
             return (
-                <Movie onDelete={this.handleDelete} onMovieUpdate={handle_update} title={movie.title} key={movie.id}
+                <Movie onDelete={this.handleDelete} onMovieUpdate={this.handleUpdate} title={movie.title} key={i}
                        year={movie.year}
                        duration={movie.duration}>
                     {movie.description}
@@ -81,11 +88,15 @@ var MoviesList = React.createClass({
  */
 var Movie = React.createClass({
     getInitialState: function () {
-        return {isEditing: false};
+        return {
+            title: this.props.title,
+            description: this.props.children,
+            year: this.props.year,
+            duration: this.props.duration
+        };
     },
 
-
-    handleClick: function (e) {
+    handleDelete: function (e) {
         e.preventDefault();
         var movieTitle = this.props.title;
         return this.props.onDelete(movieTitle);
@@ -93,8 +104,19 @@ var Movie = React.createClass({
 
     handleUpdate: function (e) {
         e.preventDefault();
-        var movie = this.props;
-        this.props.onMovieUpdate(movie);
+        var movieOld = {
+            title: this.props.title,
+            description: this.props.children,
+            year: this.props.year,
+            duration: this.props.duration
+        };
+        var movieUp = {
+            title: this.state.title,
+            description: this.state.description,
+            year: this.state.year,
+            duration: this.state.duration
+        };
+        this.props.onMovieUpdate(movieUp, movieOld);
         this.setState({isEditing: !this.state.isEditing});
     },
 
@@ -102,16 +124,32 @@ var Movie = React.createClass({
         e.preventDefault();
         this.setState({isEditing: !this.state.isEditing});
     },
+
+    handleTitleChange: function (e) {
+        this.setState({title: e.target.value.substr(0, 30)});
+    },
+    handleDescriptionChange: function (e) {
+        this.setState({description: e.target.value.substr(0, 50)});
+    },
+    handleYearChange: function (e) {
+        this.setState({year: e.target.value.substr(0, 4)});
+    },
+    handleDurationChange: function (e) {
+        this.setState({duration: e.target.value.substr(0, 3)});
+    },
     renderEditSection(){
         if (this.state.isEditing) {
             return (
                 <div className={ this.state.isEditing ?  '' : 'hidden'}>
                     <form className="commentForm" onSubmit={this.handleUpdate}>
-                        <input type="text" defaultValue={ this.props.title }/>
-                        <input type="text" defaultValue={ this.props.year }/>
-                        <input type="text" defaultValue={ this.props.children }/>
-                        <input type="text" defaultValue={ this.props.duration }/>
-                        <input  className="buttonAdd" type="submit" value="Save"/>
+                        <input type="text" defaultValue={ this.props.title } onChange={this.handleTitleChange}/>
+                        <input className="inputNumber" type="number" defaultValue={ this.props.year }
+                               onChange={this.handleYearChange}/>
+                        <input type="text" defaultValue={ this.props.children }
+                               onChange={this.handleDescriptionChange}/>
+                        <input className="inputNumber" type="number" defaultValue={ this.props.duration }
+                               onChange={this.handleDurationChange}/>
+                        <input className="buttonAdd" type="submit" value="Save"/>
                     </form>
                 </div>
             )
@@ -123,15 +161,14 @@ var Movie = React.createClass({
             <div className="moviesList">
                 <ul>
                     <li className="item">
-                        <b>{this.props.title}</b> - {this.props.year} - {this.props.children} - <i>{this.props.duration}min</i>
-                        <div id="buttons" className={ this.state.isEditing ?  'hidden':'' }>
-                            <button className="removeButton" onClick={this.handleClick}>X</button>
-                            <button className="editButton" onClick={this.toggleEditing}>Edit</button>
-                            {this.renderEditSection()}
-                        </div>
+                        <b>{this.props.title}</b> - {this.props.year} - {this.props.children} -
+                        <i>{this.props.duration}min</i>
+                        <button className="removeButton" onClick={this.handleDelete}>X</button>
+                        <button className="editButton" onClick={this.toggleEditing}>Edit</button>
+                        {this.renderEditSection()}
+                        <div className="movieImage"></div>
                     </li>
                 </ul>
-
             </div>
 
 
