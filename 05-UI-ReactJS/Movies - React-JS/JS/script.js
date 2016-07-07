19e431a7dfd7e1b7a4dd2882946aaedc4390e030
Mateo Parodi <matthparodi@gmail.com>
2016-07-06 23:52:06 -0300

    React exercise added remove button working fine

--- 05-UI-ReactJS/Movies - React-JS/JS/script.js
+++ 05-UI-ReactJS/Movies - React-JS/JS/script.js
@@ -8,15 +8,25 @@ var moviesObject = [
 
 var MoviesBox = React.createClass({
     handleMovieSubmit: function (movie) {
-        // console.log(movie);
         moviesObject.push(movie);
         this.setState({movies: moviesObject});
-        // console.log(moviesObject);
+
+    },
+    deleteMovie: function (movieTitle) {
+        for (var i = 0; i < moviesObject.length; i++) {
+            if (moviesObject[i].title === movieTitle) {
+                var movieSelected = moviesObject[i];
+            }
+        }
+
+        var index = moviesObject.indexOf(movieSelected);
+        moviesObject.splice(index, 1);
+        this.setState({movies: moviesObject});
     },
     render: function () {
         return (
             <div className="moviesBox">
-                <MoviesList movies={this.props.movies}/>
+                <MoviesList del={this.deleteMovie} movies={this.props.movies}/>
                 <MoviesForm onMovieSubmit={this.handleMovieSubmit}/>
             </div>
         );
@@ -24,14 +34,18 @@ var MoviesBox = React.createClass({
 });
 
 var MoviesList = React.createClass({
+    handleDelete: function (movieTile) {
+        return this.props.del(movieTile);
+    },
     render: function () {
         var moviesNodes = this.props.movies.map(function (movie) {
             return (
-                <Movie title={movie.title} key={movie.id}>
+                <Movie onDelete={this.handleDelete} title={movie.title} key={movie.id} year={movie.year}
+                       duration={movie.duration}>
                     {movie.description}
                 </Movie>
             );
-        });
+        }, this);
 
         return (
             <div className="moviesList">
@@ -43,24 +57,33 @@ var MoviesList = React.createClass({
 
 var MoviesForm = React.createClass({
     getInitialState: function () {
-        return {title: '', description: ''};
+        return {title: '', description: '', year: '', duration: ''};
     },
     handleTitleChange: function (e) {
-        this.setState({title: e.target.value});
+        this.setState({title: e.target.value.substr(0, 30)});
     },
     handleDescriptionChange: function (e) {
-        this.setState({description: e.target.value});
+        this.setState({description: e.target.value.substr(0, 50)});
+    },
+    handleYearChange: function (e) {
+        this.setState({year: e.target.value.substr(0, 4)});
+    },
+    handleDurationChange: function (e) {
+        this.setState({duration: e.target.value.substr(0, 3)});
     },
     handleSubmit: function (e) {
         e.preventDefault();
         var title = this.state.title.trim();
         var description = this.state.description.trim();
-        if (!description || !title) {
+        var year = this.state.year.trim();
+        var duration = this.state.duration.trim();
+        if (!description || !title || !year || !duration) {
+            alert('Please complete all the fields!')
             return;
         }
 
-        this.props.onMovieSubmit({title: title, description: description});
-        this.setState({author: '', text: ''});
+        this.props.onMovieSubmit({title: title, description: description, year: year, duration: duration});
+        this.setState({title: '', description: '', year: '', duration: ''});
     },
     render: function () {
         return (
@@ -72,12 +95,24 @@ var MoviesForm = React.createClass({
                     onChange={this.handleTitleChange}
                 />
                 <input
+                    type="number"
+                    placeholder="Release year..."
+                    value={this.state.year}
+                    onChange={this.handleYearChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Duration in min..."
+                    value={this.state.duration}
+                    onChange={this.handleDurationChange}
+                />
+                <input
                     type="text"
-                    placeholder="Movie description..."
+                    placeholder="Brief description..."
                     value={this.state.description}
                     onChange={this.handleDescriptionChange}
                 />
-                <input type="submit" value="Post"/>
+                <input type="submit" value="Add!"/>
             </form>
 
         );
@@ -85,12 +120,19 @@ var MoviesForm = React.createClass({
 });
 
 var Movie = React.createClass({
+    handleClick: function (e) {
+        e.preventDefault();
+        var movieTitle = this.props.title;
+        return this.props.onDelete(movieTitle);
+    },
+
     render: function () {
         return (
             <div className="moviesList">
                 <ul>
                     <li>
-                        <b>{this.props.title}</b> - {this.props.children}
+                        <b>{this.props.title}</b> - {this.props.year} - {this.props.children} - {this.props.duration}min
+                        <button className="removeButton" onClick={this.handleClick}>remove</button>
                     </li>
                 </ul>
             </div>
