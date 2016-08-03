08dc053b5350389c3f222b25c86fb72082f8aa94
Mateo Parodi <matthparodi@gmail.com>
2016-08-03 17:36:09 -0300

    Deleted topic2 folder

--- 02-oop-inheritance/topic2/script.js
+++
@@ -1,9 +0,0 @@
-function movie(title, year, duration){
-    this.title = title;
-    this.year = year;
-    this.duration = duration;
-}
-
-var movie1 = new movie("Terminator", "2000", "145min");
-var movie2 = new movie("Titanic", "1994", "203min");
-var movie2 = new movie("Django", "2014", "169min");
